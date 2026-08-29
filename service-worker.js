cb0b55c9fb317e666474a75be2d8e5c4a3a96fb9
icarus52-alec <icarus52-alec@users.noreply.github.com>
2026-08-29 18:02:58 +0800

    Add readable TXT export for visitors

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='media-recipe-calculator-v12';
+const CACHE='media-recipe-calculator-v13';
 const ASSETS=['./','./index.html','./manifest.json','./app-icon.png','./firebase-config.js','./firebase-sync.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
