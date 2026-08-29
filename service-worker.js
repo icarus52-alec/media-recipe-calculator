342ae26f028d5a35c684190a42b9a9950da6574c
icarus52-alec <icarus52-alec@users.noreply.github.com>
2026-08-29 15:13:03 +0800

    Add English-first language toggle

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='media-recipe-calculator-v6';
+const CACHE='media-recipe-calculator-v7';
 const ASSETS=['./','./index.html','./manifest.json','./app-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
